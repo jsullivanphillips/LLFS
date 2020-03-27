c36b784eb0d94e9226ce850388ca92fc890c5167
jsullivanphillips <jsullivanphillips@uvic.ca>
2020-03-27 13:08:09 -0700

    added the initialization of the virtual disk

--- Disk/vdisk.c
+++ Disk/vdisk.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h> 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <strings.h>
+#include <string.h>
+
 const int BLOCK_SIZE = 512;
 const int NUM_BLOCKS = 4096;
-
+const char DISKNAME[12] = "virtualDisk";
 void readBlock(FILE* disk, int blockNum, char* buffer){
 	fseek(disk, blockNum * BLOCK_SIZE, SEEK_SET);
 	fread(buffer, BLOCK_SIZE, 1, disk);
@@ -13,3 +20,32 @@ void writeBlock(FILE* disk, int blockNum, char* data){
 	fwrite(data, BLOCK_SIZE, 1, disk); //will overwrite exisitng data
 }
 
+int main (int argc, char *argv[]){
+	int ret, i, n;
+	int fd;
+	char buf[BLOCK_SIZE];
+	
+	
+	ret = open (DISKNAME, O_CREAT | O_RDWR, 0666);
+	if( ret == -1){
+		printf("could not create disk\n");
+		exit(1);
+	}
+	bzero((void*)buf, BLOCK_SIZE);
+
+	fd = open(DISKNAME, O_RDWR);
+	for(i = 0; i < NUM_BLOCKS; i++){
+		printf("block=%d\n", i);
+		n = write(fd, buf, BLOCK_SIZE);
+		if(n != BLOCK_SIZE){
+			printf("write error\n");
+			exit(1);
+		}
+	}
+
+	close(fd);
+
+	printf("created virtual disk= %s of size= %d\n", DISKNAME, BLOCK_SIZE*NUM_BLOCKS);
+
+	return(0);
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LLFS
 CSC360 Assignment3
+
+
+used github.com/hantuzun/vsfs as resource for Disk creation and formatting
